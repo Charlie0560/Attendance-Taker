ef57a527dd1ef0c8f4268b589fcd0a9f8a21dc4e
Charlie0560 <chaitanyalokhande87@gmail.com>
2022-07-18 23:47:06 +0530

    homepage

---
+++ src/components/HomePage/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './homepage.css'
+
+
+function Header() {
+  return (
+    <div className='headername'>
+        <p>ATTENDANCE TAKER</p>
+    </div>
+  )
+}
+
+export default Header
